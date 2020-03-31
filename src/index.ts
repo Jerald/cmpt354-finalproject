168d870bdfbd5d5a775fa8401546cb271a66005a
Oscar <osmithsi@sfu.ca>
2020-03-30 20:03:34 -0700

    Basic connection test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-let message: string = "hello world";
-console.log(message);
+import * as pg from "pg";
+const Client = pg.Client;
+
+async function main(): Promise<void>
+{
+    console.log(`
+    Database url: ${process.env.DATABASE_URL}
+    `);
+
+    let client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+
+    try
+    {
+        console.log("[Main] Trying to connect to client...");
+        await client.connect();
+    }
+    catch (error)
+    {
+        console.log(`[Main] Client connection error: ${error}`);
+    }
+
+    console.log("[Main] Successfully connected to client!");
+}
+
+main().then(() => {
+    console.log("SUCCESS");
+}).catch(() => {
+    console.log("FAILURE");
+});
+
