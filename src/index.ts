1acb8eb22a9c1add3435ff7a24042c1be9b248f3
Oscar <osmithsi@sfu.ca>
2020-03-30 20:34:34 -0700

    Setup test connection to heroku database

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-let message: string = "hello world";
-console.log(message);
+import * as pg from "pg";
+const Client = pg.Client;
+
+async function main(): Promise<void>
+{
+    let client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    });
+
+    try
+    {
+        console.log("[Main] Trying to connect to client...");
+        await client.connect();
+        console.log("[Main] Successfully connected to client!");
+        await client.end();
+    }
+    catch (error)
+    {
+        console.log(`[Main] Client connection error: ${error}`);
+    }
+}
+
+main().then(() => {
+    console.log("SUCCESS");
+}).catch(() => {
+    console.log("FAILURE");
+});
+
