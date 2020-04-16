a76486bd788c9a41a9d74f468374c2e2f2adfb37
hrwei2 <45253590+hrwei2@users.noreply.github.com>
2020-04-16 12:07:14 -0700

    Update q6.sql

--- sql/q6.sql
+++ sql/q6.sql
@@ -15,7 +15,8 @@ with rc as(
 SELECT r.id
 FROM researcher r, rc, cc
 WHERE r.id != rc.researcher1 AND r.id != rc.researcher2 AND r.id != rc.reviewerid AND
-r.id != cc.researcher1 AND r.id != cc.researcher2 AND r.id != cc.researcherid AND 3 > COUNT (SELECT rv.reviewerid
+r.id != cc.researcher1 AND r.id != cc.researcher2 AND r.id != cc.researcherid AND
+3 >  (SELECT COUNT(rv.reviewerid)
 FROM review rv JOIN researcher r2 ON rv.reviewerid = r2.id
 WHERE rv.reviewerid = r.id AND rv.submitted = 'false'
 GROUP BY rv.reviewerid
