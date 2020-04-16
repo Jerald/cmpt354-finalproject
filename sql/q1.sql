8645068409a7ddcd178f646fad4ba4ebc3af64e2
Oscar <osmithsi@sfu.ca>
2020-04-16 12:51:06 -0700

    Fixed query 1

--- sql/q1.sql
+++ sql/q1.sql
@@ -4,16 +4,13 @@
 -- each deadline is at the end of some month, and every call is opened at the start of the
 -- year.
 
-WITH c_count AS (
-     SELECT COUNT(*)
-     FROM collaborator C
-     WHERE C.proposalid = P.id
-), large_proposal AS (
-    SELECT *
-    FROM proposal P
-    WHERE P.callid = G.id AND (P.request_amount >= 20000.0 OR c_count > 10)
-)
 SELECT G.id, G.title
 FROM grant_call G
-WHERE EXTRACT(MONTH FROM DATE 'USER INPUT') <= EXTRACT(MONTH FROM DATE G.deadline)
-AND EXISTS (large_proposal);
+WHERE EXTRACT(MONTH FROM TIMESTAMP $1) <= EXTRACT(MONTH FROM G.deadline) AND
+EXISTS (
+    SELECT *
+    FROM proposal P
+    WHERE P.callid = G.id AND (P.request_amount >= 20000.0 OR 
+        (SELECT COUNT(*)
+        FROM collaborator C
+        WHERE C.proposalid = P.id) > 10));
