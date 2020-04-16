6cb5824fa1731dfb0337d10a4aad46f4ddc473e0
hrwei2 <45253590+hrwei2@users.noreply.github.com>
2020-04-16 11:40:26 -0700

    sql stuff
    updating sql q

---
+++ sql/q3.sql
@@ -0,0 +1,13 @@
+WITH pg AS(
+  SELECT p.id,p.request_amount
+  FROM proposal p JOIN grant_call g ON p.callid = g.id
+  WHERE g.area = $1
+)
+
+
+SELECT pg.id
+FROM pg
+WHERE pg.request_amount >= ALL (
+  SELECT pg2.request_amount
+  FROM pg AS pg2
+);
