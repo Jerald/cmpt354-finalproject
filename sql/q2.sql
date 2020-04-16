fa9efa1aa64a706bb5408fcc3649f95911484a9f
Oscar <osmithsi@sfu.ca>
2020-04-16 12:11:02 -0700

    added queries and new schema (#4)
    Added the updated schema and draft queries Q1, Q2 and Q7.
    
    Co-authored-by: Maksym Neyra-Nesterenko <37066707+VirtualTorus@users.noreply.github.com>

---
+++ sql/q2.sql
@@ -0,0 +1,21 @@
+-- CMPT354 - Project
+
+-- For Q1 (and Q2), we will assume that all the calls in the table are set in a fixed year,
+-- each deadline is at the end of some month, and every call is opened at the start of the
+-- year.
+
+WITH c_count AS (
+     SELECT COUNT(*)
+     FROM collaborator C
+     WHERE C.proposalid = P.id
+), large_proposal AS (
+    SELECT *
+    FROM proposal P
+    WHERE P.callid = G.id
+    AND (P.request_amount >= 20000.0 OR c_count > 10)
+    AND P.investigator = 'USER INPUT'
+)
+SELECT G.id, G.title
+FROM grant_call G
+WHERE EXTRACT(MONTH FROM DATE 'USER INPUT') <= EXTRACT(MONTH FROM DATE G.deadline)
+AND EXISTS (large_proposal) AND G.area = 'USER INPUT';
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,83 +1,67 @@
 CREATE TABLE researcher(
-    id SERIAL PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    organization VARCHAR(10)
+       id SERIAL PRIMARY KEY,
+       firstname TEXT NOT NULL,
+       lastname TEXT NOT NULL,
+       email TEXT UNIQUE NOT NULL,
+       organization TEXT
 );
 
-CREATE TYPE callstatus AS ENUM('open','closed','paused','cancelled'); 
+CREATE TYPE CALLSTATUS AS ENUM('open','closed');
+CREATE TYPE RESEARCH_AREA AS ENUM('Computer Science', 'Biology', 'Chemistry', 'Mathematics', 'Physics');
 
-CREATE TABLE call(
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    deadline DATE NOT NULL,
-    description VARCHAR(250),
-    area VARCHAR(30) NOT NULL,
-    status callstatus DEFAULT 'open'
+CREATE TABLE grant_call(
+       id SERIAL PRIMARY KEY,
+       title TEXT NOT NULL,
+       deadline DATE NOT NULL,
+       description TEXT,
+       area RESEARCH_AREA NOT NULL,
+       status CALLSTATUS DEFAULT 'open'
 );
 
-CREATE TYPE appstatus AS ENUM('submitted','awarded','denied');
+CREATE TYPE APPSTATUS AS ENUM('submitted','awarded','not awarded');
 
 CREATE TABLE proposal(
-    id SERIAL PRIMARY KEY,                              -- Number
-    amount NUMERIC(14,2)                                -- Requested amount
-    callid INT REFERENCES call(id) NOT NULL,            -- Call it's for
-    pi INT REFERENCES researcher(id) NOT NULL,          -- Principle investigator
-    status appstatus DEFAULT 'submitted' NOT NULL,      -- Application status
+       id SERIAL PRIMARY KEY,
+       callid INT REFERENCES grant_call(id) NOT NULL,
+       investigator INT REFERENCES researcher(id) NOT NULL,
+       status APPSTATUS DEFAULT 'submitted' NOT NULL,
+       submission_date DATE NOT NULL, -- subject to change, see https://piazza.com/class/k4f9y9b5hho5wg?cid=239
+       request_amount NUMERIC(14,2) NOT NULL,
+       awarded_amount NUMERIC(14,2)
 ); 
 
 CREATE TABLE collaborator(
-    id SERIAL PRIMARY KEY,
-    proposalid INT REFERENCES proposal(id) NOT NULL,
-    researcherid INT REFERENCES researcher(id) NOT NULL,
-    ispi BOOLEAN DEFAULT 'false' NOT NULL                   -- If the collaborator is the principle investigator
+       id SERIAL PRIMARY KEY,
+       proposalid INT REFERENCES proposal(id) NOT NULL,
+       researcherid INT REFERENCES researcher(id) NOT NULL,
+       is_investigator BOOLEAN DEFAULT 'false' NOT NULL
 );
 
 CREATE TABLE conflict(
-    id SERIAL PRIMARY KEY,
-    researcher1 INT REFERENCES researcher(id) NOT NULL,
-    researcher2 INT REFERENCES researcher(id) NOT NULL,
-    reason VARCHAR(50),
-    expiry DATE
+       id SERIAL PRIMARY KEY,
+       researcher1 INT REFERENCES researcher(id) NOT NULL,
+       researcher2 INT REFERENCES researcher(id) NOT NULL,
+       reason TEXT,
+       expiry DATE
 );
 
 CREATE TABLE review(
-    id SERIAL PRIMARY KEY,
-    reviewerid INT REFERENCES researcher(id) NOT NULL,
-    proposalid INT REFERENCES proposal(id) NOT NULL,
-    deadline DATE NOT NULL,
-    submitted BOOLEAN DEFAULT 'false' NOT NULL
+       id SERIAL PRIMARY KEY,
+       reviewerid INT REFERENCES researcher(id) NOT NULL,
+       proposalid INT REFERENCES proposal(id) NOT NULL,
+       deadline DATE NOT NULL,
+       submitted BOOLEAN DEFAULT 'false' NOT NULL
 );
 
-INSERT INTO researcher VALUES
-(DEFAULT,'Name','Lastname1','email@sfu.ca','SFU'),
-(DEFAULT,'Name','Lastname2','email@uvic.ca','UVIC');
+CREATE TABLE meeting(
+       scheduled DATE NOT NULL,
+       location TEXT NOT NULL,
+       PRIMARY KEY (scheduled, location)
+);
 
-INSERT INTO call VALUES
-(DEFAULT,'Canadian Inovation',now() + interval '2 week',NULL,'Computer Science',DEFAULT),
-(DEFAULT,'Some Title',now() + interval '3 week',NULL,'Biology',DEFAULT),
-(DEFAULT,'Reduce Carbon Footprint',now() + interval '4 week',NULL,'Engineering','closed');
-
-INSERT INTO proposal VALUES 
-(DEFAULT,2,5,'submitted',NULL),
-(DEFAULT,3,3,'denied', NULL),
-(DEFAULT,3,7,'awarded',5000.00);
-
-INSERT INTO collaborator VALUES
-(DEFAULT,1,5,'t'),
-(DEFAULT,1,1,'f'),
-(DEFAULT,2,7,'t'),
-(DEFAULT,3,5,'t');
-
-INSERT INTO conflict VALUES
-(DEFAULT,1,2,'co-authered paper',now() + interval '2 year'),
-(DEFAULT,4,5,'related',NULL),
-(DEFAULT,3,7,'Same Department',NULL);
-
-INSERT INTO review VALUES
-(DEFAULT,6,3,now(),'t'),
-(DEFAULT,7,1,now() + interval '2 week','f'),
-(DEFAULT,6,1,now() + interval '2 week','t'),
-(DEFAULT,1,2,now(),'t');
-
+CREATE TABLE meeting_calls(
+       calls INT REFERENCES grant_call(id)
+       meet_date DATE,
+       meet_loc TEXT,
+       FOREIGN KEY (meet_date, meet_loc) REFERENCES meeting(scheduled, location)
+);
