36799a45e7438b507f82fc66dde8b0ab7da96513
hrwei2 <45253590+hrwei2@users.noreply.github.com>
2020-04-20 20:33:19 -0700

    Update q4.sql
    q4 fixed

--- sql/q4.sql
+++ sql/q4.sql
@@ -1,14 +1,14 @@
 -- $1: Date, in UNIX time
 
 WITH pg AS (
-  SELECT p.id,p.request_amount
-  FROM proposal p JOIN grant_call g ON p.callid = g.id
-  WHERE to_timestamp($1) < p.submission_date
+  SELECT p.id,p.awarded_amount
+  FROM proposal p
+  WHERE to_timestamp($1, 'YYYY-MM-DD') > p.submission_date AND p.status = 'awarded'
 )
 
 SELECT pg.id
 FROM pg
-WHERE pg.request_amount >= ALL (
-  SELECT pg2.request_amount
-  FROM pg AS pg2
+WHERE pg.awarded_amount >= ALL(
+  SELECT pg2.awarded_amount
+  FROM pg as pg2
 );
