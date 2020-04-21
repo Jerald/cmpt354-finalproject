aa90d61025d4c7ff0da3a7b0a35d34ee4881b149
Maksym Neyra-Nesterenko <37066707+VirtualTorus@users.noreply.github.com>
2020-04-21 15:04:49 -0700

    Create q6-insert.sql (#15)
    * Create q6-insert.sql
    
    INSERT part for Q6 to be used in a loop.
    
    * Update and rename q6.sql to q6-query.sql

---
+++ sql/q6-insert.sql
@@ -0,0 +1,8 @@
+WITH D AS (
+  SELECT G.deadline + integer '14' AS deadline
+  FROM proposal P JOIN grant_call G ON P.callid = G.id
+  WHERE P.proposalid = $1
+)
+
+INSERT INTO review (reviewerid, proposalid, deadline)
+VALUES ($2, $1, D);
