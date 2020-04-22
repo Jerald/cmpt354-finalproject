4d831be65975a15367a62cfcfafb25a079eb4e1f
Maksym Neyra-Nesterenko <37066707+VirtualTorus@users.noreply.github.com>
2020-04-21 18:11:04 -0700

    fixed sql statements
    Fixed SQL statements to be submitted in the project.

--- WeiHaoranNeyra-NesterenkoMaksymSmith-SiegerOscar-Commands/WeiHaoranNeyra-NesterenkoMaksymSmith-SiegerOscar-Drop.sql
+++ WeiHaoranNeyra-NesterenkoMaksymSmith-SiegerOscar-Commands/WeiHaoranNeyra-NesterenkoMaksymSmith-SiegerOscar-Drop.sql
@@ -1,9 +1,13 @@
-DROP TABLE meeting;
 DROP TABLE meeting_attendees;
 DROP TABLE meeting_calls;
+DROP TABLE meeting;
 DROP TABLE review;
 DROP TABLE conflict;
 DROP TABLE collaborator;
 DROP TABLE proposal;
 DROP TABLE grant_call;
 DROP TABLE researcher;
+
+DROP TYPE callstatus;
+DROP TYPE research_area;
+DROP TYPE appstatus;
